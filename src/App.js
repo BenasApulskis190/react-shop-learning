c9580ced608c7bb214db18ce5d192f4c09be9281
Benas Apulskis <benas@ito.lt>
2020-05-21 23:26:05 +0300

    Master added router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
-import './App.scss';
 import HomePage from './pages/homepage/homepage.component';
+import { Route, Switch } from 'react-router-dom'
+import './App.scss';
 
 function App() {
-  return <div><HomePage /></div>;
+  return (
+    <div>
+      <Switch>
+        <Route exact path='/' component={HomePage}/>
+      </Switch>
+    </div>);
 }
 
 export default App;
